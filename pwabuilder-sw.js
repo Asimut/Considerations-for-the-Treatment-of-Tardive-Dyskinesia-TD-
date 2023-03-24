dbc208131ac866416d0fae296aba490c7873b514
Asimut <asimut007@gmail.com>
2023-03-24 18:14:30 +0200

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -8,7 +8,7 @@ var CURRENT_CACHES = {
 self.addEventListener('install', e => {
   console.log('11111111111111111111');
 
-  e.waitUntil(caches.open(CURRENT_CACHES).then(async (cache) => {
+  e.waitUntil(caches.open(CURRENT_CACHES.prefetch).then(async (cache) => {
     let ok,
     libjs = ['player-0.0.11.min', 'main.bundle', 'lzwcompress'],
     libcss = ['main.bundle', 'icomoon'],
@@ -108,18 +108,18 @@ self.addEventListener('install', e => {
       '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/custom/ingrezza-valbenazine-logo-n.svg',
       '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/custom/logo-modal.png',
       '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/custom/open-book.svg',
-      ...assetsvideo.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.mp4'),
-      ...assetssvg.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.svg'),
-      ...assetspng.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.png'),
-      ...assetsjpg.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.jpg'),
-      ...assetspdf.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.pdf'),
-      '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/pwabuilder-sw.js',
-      '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/manifest.json',
-      '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/152.png',
-      '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/144.png',
-      '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/64.png',
-      '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/32.png',
-      '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/android-launchericon-512-512.png'
+      // ...assetsvideo.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.mp4'),
+      // ...assetssvg.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.svg'),
+      // ...assetspng.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.png'),
+      // ...assetsjpg.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.jpg'),
+      // ...assetspdf.map(i => '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/assets/' + i + '.pdf'),
+      // '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/pwabuilder-sw.js',
+      // '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/manifest.json',
+      // '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/152.png',
+      // '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/144.png',
+      // '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/64.png',
+      // '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/32.png',
+      // '/Considerations-for-the-Treatment-of-Tardive-Dyskinesia-TD-/android-launchericon-512-512.png'
       ];
 
     console.log('ServiceWorker: Caching files:', c.length, c);
